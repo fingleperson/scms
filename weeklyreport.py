235174ae929a6cb59ef7c037a1902d94ac1a0827
fingleperson <elihcarlson@gmail.com>
2025-08-13 10:58:20 -0700

    Update weeklyreport.py

--- weeklyreport.py
+++ weeklyreport.py
@@ -6,40 +6,58 @@ API_KEY = os.getenv("OPEN_CLOUD_API_KEY")
 ID = os.getenv("ID")
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
 DATASTORE_NAME = "analyticsdata"
-STAT_KEYS = ["EARNED", "SPENT", "TOTAL"]
+STAT_KEYS = ["earned", "spent"]
 
 def get_stat(stat_name):
     url = f"https://apis.roblox.com/datastores/v1/universes/{ID}/standard-datastores/datastore/entries/entry"
-    params = {"datastoreName": DATASTORE_NAME, "scope": "", "entryKey": stat_name}
+    params = {"datastoreName": DATASTORE_NAME, "entryKey": stat_name}
     headers = {"x-api-key": API_KEY}
     r = requests.get(url, headers=headers, params=params)
     if r.status_code == 200:
-        return int(r.json().get("value", 0))
+        return float(r.json().get("value", 0))
     return 0
 
 def set_stat(stat_name, value):
     url = f"https://apis.roblox.com/datastores/v1/universes/{ID}/standard-datastores/datastore/entries/entry"
-    params = {"datastoreName": DATASTORE_NAME, "scope": "", "entryKey": stat_name}
+    params = {"datastoreName": DATASTORE_NAME, "entryKey": stat_name}
     headers = {
         "x-api-key": API_KEY,
         "Content-Type": "application/json"
     }
     requests.post(url, headers=headers, params=params, json={"value": value})
 
-def post(stats):
+def post():
+    earned = float(get_stat("earned"))
+    spent = float(get_stat("spent"))
+
     embed = {
         "title": "WEEKLY REVENUE REPORT",
         #"description": f"FROM {date_str} (UTC)",
+
         "fields": [
-            {"name": key, "value": str(value), "inline": True} for key, value in stats.items()
-            ],
+            {
+                "name": "EARNED",
+                "value": earned,
+                "inline": True
+            },
+            {
+                "name": "SPENT",
+                "value": spent,
+                "inline": True
+            },
+            {
+                "name": "TOTAL",
+                "value": earned + spent,
+                "inline": True
+            }
+        ],
+
         "timestamp": datetime.datetime.now(tz=datetime.UTC).isoformat()
     }
     payload = {"embeds": [embed]}
     requests.post(DISCORD_WEBHOOK_URL, json=payload)
 
 if __name__ == "__main__":
-    stats = {key: get_stat(key) for key in STAT_KEYS}
-    post(stats)
+    post()
     for key in STAT_KEYS:
         set_stat(key, 0)
