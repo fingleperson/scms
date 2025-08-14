4921b20c2900c21586f60e2b1dd4b7ffa57dac71
fingleperson <elihcarlson@gmail.com>
2025-08-14 10:31:57 -0700

    Update weeklyreport.py

--- weeklyreport.py
+++ weeklyreport.py
@@ -77,7 +77,8 @@ def post():
         "timestamp": datetime.datetime.now(tz=datetime.UTC).isoformat()
     }
     payload = {"content": "<@&1405580107992662136>", "embeds": [embed]}
-    requests.post(DISCORD_WEBHOOK_URL, json=payload, verify=True)
+    r = requests.post(DISCORD_WEBHOOK_URL, json=payload, verify=True)
+    print(r.response_code, r.text)
 
 if __name__ == "__main__":
     post()
